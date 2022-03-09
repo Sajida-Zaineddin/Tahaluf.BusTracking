97432dd6b5d5b79bcca2c8b3b691fec0081b55a5
AseelAburumman <aseelaburumman10@gmail.com>
2022-03-09 22:35:50 +0300

    websiteFooter

---
+++ Tahaluf.BusTracking.Core/Repository/IWebsitefooterRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.BusTracking.Core.Data;
+
+namespace Tahaluf.BusTracking.Core.Repository
+{
+    public interface IWebsitefooterRepository
+    {
+        List<Websitefooter> GetAllwebsitefooter();
+        bool Createwebsitefooter(Websitefooter websitefooter);
+        bool Updatewebsitefooter(Websitefooter websitefooter);
+        bool Deletewebsitefooter(int Id);
+
+    }
+}
---
+++ Tahaluf.BusTracking.Infra/Service/WebsitefooterService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.BusTracking.Core.Data;
+using Tahaluf.BusTracking.Core.Repository;
+using Tahaluf.BusTracking.Core.Service;
+
+namespace Tahaluf.BusTracking.Infra.Service
+{
+    public class WebsitefooterService : IWebsitefooterService
+    {
+        private readonly IWebsitefooterRepository websitefooterRepository;
+
+        public WebsitefooterService(IWebsitefooterRepository _websitefooterRepository)
+        {
+            websitefooterRepository = _websitefooterRepository;
+        }
+        public List<Websitefooter> GetAllwebsitefooter()
+        {
+            return websitefooterRepository.GetAllwebsitefooter();
+        }
+        public bool Createwebsitefooter(Websitefooter websitefooter)
+        {
+            return websitefooterRepository.Createwebsitefooter(websitefooter);    
+        }
+        public bool Updatewebsitefooter(Websitefooter websitefooter)
+        {
+            return websitefooterRepository.Updatewebsitefooter(websitefooter);
+        }
+        public bool Deletewebsitefooter(int Id)
+        {
+            return websitefooterRepository.Deletewebsitefooter(Id);
+        }
+    }
+}
