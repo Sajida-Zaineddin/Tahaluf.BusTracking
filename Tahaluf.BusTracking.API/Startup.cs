2abf47c4312679d7347421007f485e65b89e01ef
Sajida Zaineddin <1837240@std.hu.edu.jo>
2022-03-14 17:49:05 +0300

    update startup

--- Tahaluf.BusTracking.API/Startup.cs
+++ Tahaluf.BusTracking.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,9 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Tahaluf.BusTracking.Core.Common;
 using Tahaluf.BusTracking.Core.Repository;
@@ -33,6 +36,9 @@ namespace Tahaluf.BusTracking.API
         {
             services.AddControllers();
             services.AddScoped<IDbContext, DbContext>();
+            //Aboutus
+            services.AddScoped<IAboutusRepository, AboutusRepository>();
+            services.AddScoped<IAboutusService, AboutusService>();
             //AboutusEditor
             services.AddScoped<IAboutusEditorRepository, AboutusEditorRepository>();
             services.AddScoped<IAboutusEditorService, AboutusEditorService>();
@@ -69,10 +75,31 @@ namespace Tahaluf.BusTracking.API
             //WebsiteHome
             services.AddScoped<IWebsitehomeRepository, WebsitehomeRepository>();
             services.AddScoped<IWebsitehomeService, WebsitehomeService>();
+            //Login
+            services.AddScoped<ILoginRepository, LoginRepository>();
+            services.AddScoped<ILoginService, LoginService>();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    //ValidIssuer = "http://localhost:5000",
+                    //ValidAudience = "http://localhost:5000",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("welcome to eqbal site"))
+                };
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
