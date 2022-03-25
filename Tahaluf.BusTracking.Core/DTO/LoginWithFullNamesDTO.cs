74280f6ad3fb5e0e21d098b5c1a46ce0edbab217
eqbal97 <eqbal.saleh7@gmail.com>
2022-03-25 23:16:19 +0300

    login manage

---
+++ Tahaluf.BusTracking.Core/DTO/LoginWithFullNamesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.BusTracking.Core.DTO
+{
+    public class LoginWithFullNamesDTO
+    {
+        public decimal Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string password { get; set; }
+        public string FullName { get; set; }
+
+        public decimal? Userid { get; set; }
+
+    }
+}
--- Tahaluf.BusTracking.Core/Service/ILoginService.cs
+++ Tahaluf.BusTracking.Core/Service/ILoginService.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 using Tahaluf.BusTracking.Core.Data;
+using Tahaluf.BusTracking.Core.DTO;
 
 namespace Tahaluf.BusTracking.Core.Service
 {
     public interface ILoginService
     {
         String Auth(Login login);
+
+        bool CreateLoginUser(Login login);
+
+        bool UpdateLoginUser(Login login);
+
+        string DeleteLoginUser(int id);
+
+        List<LoginWithFullNamesDTO> GetAllUsersWithNames();
+
     }
 }
